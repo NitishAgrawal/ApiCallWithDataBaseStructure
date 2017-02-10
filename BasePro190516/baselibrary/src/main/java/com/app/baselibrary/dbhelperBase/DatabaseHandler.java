68467b0bc1172a7823c8c049a7ebfd58e5620c45
NitishAgrawal <agrawal.nitish3008@gmail.com>
2017-02-10 17:44:28 +0530

    Add alter table code

--- BasePro190516/baselibrary/src/main/java/com/app/baselibrary/dbhelperBase/DatabaseHandler.java
+++ BasePro190516/baselibrary/src/main/java/com/app/baselibrary/dbhelperBase/DatabaseHandler.java
@@ -1,6 +1,7 @@
 package com.app.baselibrary.dbhelperBase;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -57,14 +58,70 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     // Upgrading database
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        for (Map.Entry<String, HashMap<String,String>> entry : dbSchema.entrySet())
+        if(newVersion > oldVersion) {
+            alterTableIfRequired(db, true);
+        }
+        /*for (Map.Entry<String, HashMap<String,String>> entry : dbSchema.entrySet())
         {
             String tableName = entry.getKey();
             db.execSQL("DROP TABLE IF EXISTS " + tableName);
+
         }
 		// Create tables again
-		onCreate(db);
-        Log.e("Database", "working3");
+		onCreate(db);*/
 	}
+
+    public void alterTableIfRequired(SQLiteDatabase mDatabase, boolean openDb) {
+
+        if(openDb) {
+            if(mDatabase == null || !mDatabase.isOpen()) {
+                mDatabase = getReadableDatabase();
+            }
+
+            if(!mDatabase.isReadOnly()) {
+                mDatabase.close();
+                mDatabase = getReadableDatabase();
+            }
+        }
+        for (Map.Entry<String, HashMap<String,String>> entry : dbSchema.entrySet())
+        {
+            String tableName = entry.getKey();
+            Cursor cursor = mDatabase.rawQuery("select DISTINCT tbl_name from sqlite_master where tbl_name = '"+tableName+"'", null);
+            String addColumn = "";
+            String deleteColumn = " ";
+
+            if(cursor!=null) {
+                if(cursor.getCount()>0) {
+                    HashMap<String,String> tableFields = entry.getValue();
+                    String sapreter = "";
+                    for (Map.Entry<String,String> entry1 : tableFields.entrySet()) {
+                        String key = entry1.getKey();
+                        Object value = entry1.getValue();
+                        if(cursor.getColumnIndex(key) == -1){
+                            addColumn = sapreter+key+" "+value;
+                            sapreter = ", ";
+                        }
+                    }
+                    String[] columnNames = cursor.getColumnNames();
+                    sapreter = "";
+                    for(String column:columnNames){
+                        if(!tableFields.containsKey(column)){
+                            addColumn = sapreter+column;
+                            sapreter = ", ";
+                        }
+                    }
+                    cursor.close();
+                }
+                cursor.close();
+                if(addColumn.length()>0) {
+                    mDatabase.execSQL("ALTER TABLE "+tableName+" ADD COLUMN ("+addColumn+")");
+                }
+                if(deleteColumn.length()>0) {
+                    mDatabase.execSQL("ALTER TABLE "+tableName+" DROP COLUMN ("+deleteColumn+")");
+                }
+
+            }
+        }
+    }
 
 }
